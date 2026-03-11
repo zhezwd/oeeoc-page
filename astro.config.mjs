27de4456e2e2210a47617f0a101c13aba9a78f98
Yang Jiabao <163554725+zhezwd@users.noreply.github.com>
2026-03-11 20:40:56 +0800

    Remove sitemap integration from astro.config.mjs
    Temporarily remove sitemap integration due to errors.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+// 暂时移除报错的 sitemap 插件
+// import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://oeeoc.pages.dev',
   output: 'static',
-  integrations: [mdx(), sitemap()],
+  // 这里去掉了 sitemap()
+  integrations: [mdx()],
   build: {
     format: 'directory',
     assets: 'assets'
